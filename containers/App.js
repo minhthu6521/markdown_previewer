ce7e2d98fbc5fb6af1eb444fb71b4e07f8dbb039
Thu Nguyen <minhthu6521@gmail.com>
2018-06-03 23:44:59 +0300

    raw text -> html

---
+++ containers/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+import Result from './Result.js'
+export default class App extends Component {
+  render() {
+    return (<Result />)
+  }
+}
---
+++ containers/Result.js
@@ -0,0 +1,34 @@
+import React, {
+	Component
+} from 'react';
+
+const InputComponent =  ({updateValue, setValue}) =>  {return (<textarea rows="22" type="text" value={setValue} className="form-control" onChange={updateValue} />)}
+
+export default class Result extends Component {
+	constructor() {
+		super();
+		this.state = {
+			value: "test",
+		};
+	}
+	updateValue(e) {
+    this.setState({
+        value: e.target.value
+    })
+  }
+  rawMarkup(value) {
+    var rawMarkup = marked(value, {sanitize: true});
+    return { __html: rawMarkup };
+  }
+
+	render() {
+		return ( <div className="row">
+      <div className="col-md-6">
+        <InputComponent updateValue={this.updateValue.bind(this)} setValue={this.state.value} />
+      </div>
+      <div className="col-md-6">
+        <span dangerouslySetInnerHTML={this.rawMarkup(this.state.value)} />
+      </div>
+    </div> )
+	}
+}
